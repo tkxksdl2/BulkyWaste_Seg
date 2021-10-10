0c3320b7b85dcd8fdee66e3eeeed5ce23cb356a7
airhac <oooh32141@gmail.com>
2021-10-10 09:14:42 +0900

    10월 10일 commit1 main url에 articleapp 경로 추가

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'djangoProject1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
